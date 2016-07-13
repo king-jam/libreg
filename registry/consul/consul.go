166bfe35af7266048c7ac966f7b49754da752a94
James King <james.king@emc.com>
2016-07-13 15:40:23 -0400

    Type COnversions

--- registry/consul/consul.go
+++ registry/consul/consul.go
@@ -4,7 +4,6 @@ import (
 	"crypto/tls"
 	"errors"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
@@ -38,7 +37,7 @@ func Register() {
 
 // New creates a new Consul client given a list
 // of endpoints and optional tls config
-func New(endpoints []string, options *store.Config) (store.Store, error) {
+func New(endpoints []string, options *registry.Config) (registry.Registry, error) {
 	if len(endpoints) > 1 {
 		return nil, ErrMultipleEndpointsUnsupported
 	}
@@ -85,50 +84,125 @@ func (s *Consul) setTimeout(time time.Duration) {
 	s.config.WaitTime = time
 }
 
-func (s *Consul) Register(reg *CatalogRegistration, options *WriteOptions) error {
-	catalog := s.api.Catalog()
-	_, err := catalog.Register(reg, options)
+func (s *Consul) Register(reg *registry.CatalogRegistration, options *registry.WriteOptions) error {
+	catalog := s.client.Catalog()
+	_, err := catalog.Register(
+		&api.CatalogRegistration{
+			Node:       reg.Node,
+			Address:    reg.Address,
+			Datacenter: reg.Datacenter,
+			Service:    &api.AgentService{},
+			Check:      &api.AgentCheck{},
+		},
+		&api.WriteOptions{
+			Datacenter: options.Datacenter,
+			Token:      options.Token,
+		})
 	return err
 }
 
 // Deregister removes a node, service or check
-func (s *Consul) Deregister(dereg *CatalogDeregistration, options *WriteOptions) error {
-	catalog := s.api.Catalog()
-	_, err := catalog.Deregister(dereg, options)
+func (s *Consul) Deregister(dereg *registry.CatalogDeregistration, options *registry.WriteOptions) error {
+	catalog := s.client.Catalog()
+	_, err := catalog.Deregister(
+		&api.CatalogDeregistration{
+			Node:       dereg.Node,
+			Address:    dereg.Address,
+			Datacenter: dereg.Datacenter,
+			ServiceID:  dereg.ServiceID,
+			CheckID:    dereg.CheckID,
+		},
+		&api.WriteOptions{
+			Datacenter: options.Datacenter,
+			Token:      options.Token,
+		})
 	return err
 }
 
 // Datacenters lists known datacenters
 func (s *Consul) Datacenters() ([]string, error) {
-	catalog := s.api.Catalog()
+	catalog := s.client.Catalog()
 	dc, err := catalog.Datacenters()
 	return dc, err
 }
 
 // Nodes lists all nodes in a given DC
-func (s *Consul) Nodes(options *QueryOptions) ([]*Node, error) {
-	catalog := s.api.Catalog()
-	nodes, _, err := catalog.Nodes(options)
-	return nodes, err
+func (s *Consul) Nodes(options *registry.QueryOptions) ([]*registry.Node, error) {
+	catalog := s.client.Catalog()
+	nodes, _, err := catalog.Nodes(
+		&api.QueryOptions{})
+	var retNodes []*registry.Node
+	for _, v := range nodes {
+		retNodes = append(retNodes, &registry.Node{
+			Node:    v.Node,
+			Address: v.Address,
+		})
+	}
+	return retNodes, err
 }
 
 // Services lists all services in a given DC
-func (s *Consul) Services(options *QueryOptions) (map[string][]string, error) {
-	catalog := s.api.Catalog()
-	services, _, err := catalog.Services(options)
+func (s *Consul) Services(options *registry.QueryOptions) (map[string][]string, error) {
+	catalog := s.client.Catalog()
+	services, _, err := catalog.Services(
+		&api.QueryOptions{})
 	return services, err
 }
 
 // Service lists the nodes in a given service
-func (s *Consul) Service(service, tag string, options *QueryOptions) ([]*CatalogService, error) {
-	catalog := s.api.Catalog()
-	services, _, err := catalog.Services(service, tag, options)
-	return services, err
+func (s *Consul) Service(service, tag string, options *registry.QueryOptions) ([]*registry.CatalogService, error) {
+	catalog := s.client.Catalog()
+	services, _, err := catalog.Service(
+		service,
+		tag,
+		&api.QueryOptions{})
+	var retServices []*registry.CatalogService
+	for _, v := range services {
+		retServices = append(retServices, &registry.CatalogService{
+			Node:                     v.Node,
+			Address:                  v.Address,
+			ServiceID:                v.ServiceID,
+			ServiceName:              v.ServiceName,
+			ServiceAddress:           v.ServiceAddress,
+			ServiceTags:              v.ServiceTags,
+			ServicePort:              v.ServicePort,
+			ServiceEnableTagOverride: v.ServiceEnableTagOverride,
+		})
+	}
+	return retServices, err
 }
 
 // Node lists the services provided by a given node
-func (s *Consul) Node(node string, options *QueryOptions) (*CatalogNode, error) {
-	catalog := s.api.Catalog()
-	node, _, err := catalog.Node(node, options)
-	return node, err
+func (s *Consul) Node(node string, options *registry.QueryOptions) (*registry.CatalogNode, error) {
+	catalog := s.client.Catalog()
+	n, _, err := catalog.Node(
+		node,
+		&api.QueryOptions{
+			Datacenter:        options.Datacenter,
+			AllowStale:        options.AllowStale,
+			RequireConsistent: options.RequireConsistent,
+			WaitIndex:         options.WaitIndex,
+			WaitTime:          options.WaitTime,
+			Token:             options.Token,
+			Near:              options.Near,
+		})
+	var retNode *registry.Node = &registry.Node{
+		Node:    n.Node.Node,
+		Address: n.Node.Address,
+	}
+	var retService = make(map[string]*registry.AgentService)
+	for k, v := range n.Services {
+		retService[k] = &registry.AgentService{
+			ID:                v.ID,
+			Service:           v.Service,
+			Tags:              v.Tags,
+			Port:              v.Port,
+			Address:           v.Address,
+			EnableTagOverride: v.EnableTagOverride,
+		}
+	}
+	return &registry.CatalogNode{
+		Node:     retNode,
+		Services: retService,
+	}, err
 }
